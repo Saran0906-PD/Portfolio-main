50ea3beee6ad7f1abff92b1d15b3b457e98437a9
Saran0906-PD <Sarangitacc.com>
2026-03-02 01:34:51 +0530

    new

--- Portfolio-main/src/components/Footer.jsx
+++ Portfolio-main/src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       {" "}
       <p className="text-sm text-muted-foreground">
         {" "}
-        &copy; {new Date().getFullYear()} All rights reserved.
+        &copy; {new Date().getFullYear()} All rights reservedd.
       </p>
       <a
         href="#hero"
